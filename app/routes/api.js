4e8bb595f0c267188cf8e282a87d929bcf07779d
Jeremy Moore <jeremymoore@gmail.com>
2013-11-21 23:56:35 -0800

    q-promise implementation working sans elevation

--- app/routes/api.js
+++ app/routes/api.js
@@ -34,7 +34,6 @@ function deferredConnect() {
 
 function deferredQuery(client, queryString) {
   var deferred = Q.defer();
-  console.log("client is " + client);
   client.query(queryString, function(err, result) {
     if (err) {
       deferred.reject(new Error("An error has occurred."));
@@ -80,51 +79,34 @@ exports.watershed = function(req, res){
   var latitude = req.query.latitude;
   var longitude = req.query.longitude;
   Q.when(validateRequest(req), function(resp) {
-    var db_client = deferredConnect().spread(function(client, done) {
-        for (var prop in client) {
-          console.log(prop);
-        }
-        return client;
-      }, function(error) {
-        throw new Error("Could not connect to client");
-      });
-    var results = deferredQuery(client_done[0], [
-        "select ",
-        "sfp_watershed.name1, sfp_watershed.facility, ",
-        "ST_X(ST_TRANSFORM(ST_CENTROID(sf_wastewater_plant.geom), 4269)), ",
-        "ST_Y(ST_TRANSFORM(ST_CENTROID(sf_wastewater_plant.geom), 4269)), ",
-        "ST_DISTANCE(ST_CENTROID(sf_wastewater_plant.geom), ST_TRANSFORM(ST_PointFromText('POINT(" + longitude + " " + latitude + ")', 4269), 2227)) ",
-        "from sfp_watershed left join sf_wastewater_plant ",
-        "on sfp_watershed.facility = sf_wastewater_plant.name1 ",
-        "where ST_Contains(sfp_watershed.geom, ST_TRANSFORM(ST_PointFromText('POINT(" + longitude + " " + latitude + ")', 4269), 2227))"
-      ].join("")).then(function(query_results) {
-        client_done[1]();
-        return query_results;
-      }, function(error) {
-        throw error;
-      });
-    if (result.rows.length > 0) {
-      console.log(result.rows[0]);
-      data.watershed = result.rows[0].name1; 
-      data.facility = {
-          longitude: result.rows[0].st_x,
-          latitude: result.rows[0].st_y,
-          name: result.rows[0].facility,
-          distance: result.rows[0].st_distance/5280.0};
-      var elevation = getElevation(latitude, longitude).then(function(elevation) {
-          return elevation;
-        }, function(error) {
-          return null;
+    return deferredConnect().spread(function(client, done) {
+      return deferredQuery(client, [
+          "select ",
+          "sfp_watershed.name1, sfp_watershed.facility, ",
+          "ST_X(ST_TRANSFORM(ST_CENTROID(sf_wastewater_plant.geom), 4269)), ",
+          "ST_Y(ST_TRANSFORM(ST_CENTROID(sf_wastewater_plant.geom), 4269)), ",
+          "ST_DISTANCE(ST_CENTROID(sf_wastewater_plant.geom), ST_TRANSFORM(ST_PointFromText('POINT(" + longitude + " " + latitude + ")', 4269), 2227)) ",
+          "from sfp_watershed left join sf_wastewater_plant ",
+          "on sfp_watershed.facility = sf_wastewater_plant.name1 ",
+          "where ST_Contains(sfp_watershed.geom, ST_TRANSFORM(ST_PointFromText('POINT(" + longitude + " " + latitude + ")', 4269), 2227))"
+        ].join("")).then(function(result) {
+          done();
+          if (result.rows.length > 0) {
+            data = {};
+            data.watershed = result.rows[0].name1;
+            data.facility = {
+                longitude: result.rows[0].st_x,
+                latitude: result.rows[0].st_y,
+                name: result.rows[0].facility,
+                distance: result.rows[0].st_distance/5280.0};
+            return Q.resolve(data);
+          } else {
+            throw new Error("No results for location.");
+          }
+        }).then(function(response) {
+          res.send(response);
         });
-      if (elevation !== null) {
-        data.elevation = elevation;
-      }
-      return deferred.resolve(data);
-    } else {
-      throw new Error("No results for location.");
-    }
-  }).then(function(response) {
-    res.send(response);
+    });
   }, function(error) {
     res.send({error: error.message});
   });
